4eb7d5c2f64fcc1727737d32a70aa060f026d263
milantiejiang <1152909011@qq.com>
2019-05-14 10:51:50 +0800

    first commit

---
+++ MyGame/LShape.swift
@@ -0,0 +1,59 @@
+//
+//  LShape.swift
+//  MyGame
+//
+//  Created by 米兰铁匠 on 2019/5/4.
+//  Copyright © 2019年 米兰铁匠. All rights reserved.
+//
+
+import Foundation
+class LShape:Shape {
+    /*
+     
+     Orientation 0
+     
+     | 0•|
+     | 1 |
+     | 2 | 3 |
+     
+     Orientation 90
+     
+     •
+     | 2 | 1 | 0 |
+     | 3 |
+     
+     Orientation 180
+     
+     | 3 | 2•|
+     | 1 |
+     | 0 |
+     
+     Orientation 270
+     
+     • | 3 |
+     | 0 | 1 | 2 |
+     
+     • marks the row/column indicator for the shape
+     
+     Pivots about `1`
+     
+     */
+    
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero:       [ (0, 0), (0, 1),  (0, 2),  (1, 2)],
+            Orientation.Ninety:     [ (1, 1), (0, 1),  (-1,1), (-1, 2)],
+            Orientation.OneEighty:  [ (0, 2), (0, 1),  (0, 0),  (-1,0)],
+            Orientation.TwoSeventy: [ (-1,1), (0, 1),  (1, 1),   (1,0)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[FirstBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[ThirdBlockIdx]]
+        ]
+    }
+}
